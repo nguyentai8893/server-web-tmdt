2773403172da35bed6cb99b7185e2a8a22c0137d
nguyentai8893 <nguyenvantai566@gmail.com>
2024-05-24 23:50:33 +0700

    ok

--- src/Middleware/uploadMiddleware.js
+++ src/Middleware/uploadMiddleware.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
+		console.log('file:::/n', file);
 		cb(null, path.join('src/public/uploads'));
 	},
 	filename: function (req, file, cb) {
