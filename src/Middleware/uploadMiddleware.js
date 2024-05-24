6dd6534f64f9aec3bce4181507e4dd95d6b128f2
nguyentai8893 <nguyenvantai566@gmail.com>
2024-05-25 00:08:46 +0700

    ok

--- src/Middleware/uploadMiddleware.js
+++ src/Middleware/uploadMiddleware.js
@@ -3,7 +3,7 @@ const path = require('path');
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		console.log('file:::/n', file);
-		cb(null, path.join('src/public/uploads'));
+		cb(null, path.join(__dirname, './public/uploads'));
 	},
 	filename: function (req, file, cb) {
 		// Tên file mới được tạo ra, bao gồm thời gian và phần mở rộng của file gốc
